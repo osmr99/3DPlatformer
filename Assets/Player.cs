31e178d4b3f71b625b84c813d35fee157b9290ea
osmr99 <omarsantiagomorales@gmail.com>
2024-03-07 23:38:38 -0600

    new movement system with camera

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     float moveSpeed = 10;
-    float jumpPower = 7.5f;
+    //float jumpPower = 7.5f;
+    float jumpPower = 5;
     bool onGround = false;
     float groundCheckDistance = 0.5f;
+    float forwardMovementInput;
+    float rightMovementInput;
     Rigidbody rb;
+
+    Transform cam;
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, 10, 0);
         rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
@@ -27,15 +34,40 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
         }
-        
 
 
-        float xMovement = Input.GetAxis("Horizontal") * moveSpeed;
-        Vector3 xAmountH = Vector3.left * xMovement;
-        transform.position += xAmountH * Time.deltaTime;
 
-        float zMovement = Input.GetAxis("Vertical") * moveSpeed;
-        Vector3 zAmount = Vector3.back * zMovement;
-        transform.position += zAmount * Time.deltaTime;
+
+        rightMovementInput = Input.GetAxis("Horizontal2") * moveSpeed;
+        //Vector3 zAmount = Vector3.back * zMovement;
+        //transform.position += zAmount * Time.deltaTime;
+
+        forwardMovementInput = Input.GetAxis("Vertical2") * moveSpeed;
+        //Vector3 xAmountH = Vector3.left * xMovement;
+        //transform.position += xAmountH * Time.deltaTime;
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0;
+        camForward.Normalize();
+
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 forwardRelative = forwardMovementInput * camForward;
+        Vector3 rightRelative = rightMovementInput * camRight;
+
+        Vector3 movementVector = (forwardRelative + rightRelative).normalized * moveSpeed;
+
+        movementVector.y = rb.velocity.y;
+
+        rb.velocity = movementVector;
+
+    }
+
+    private void FixedUpdate()
+    {
+
     }
 }
